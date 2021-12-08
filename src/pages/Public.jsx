d8a5436927f12f88164bb5e418df9643b1ad65af
juanfed <juanfed12345@mail.com>
2021-12-07 21:13:15 -0500

    solucion a la hora de visualizar en laptos con menos resolucion

--- src/pages/Public.jsx
+++ src/pages/Public.jsx
@@ -7,16 +7,18 @@ const Public = () => {
         <div className="public">
             <div className="public__main">
                 <div className="public__card1">
-                    <div className="public__card1--tittle">
-                        <h1>Proyectos S.A.S</h1>
-                    </div>
-                    <div className="public__card1--text">
-                        <p>
-                            Somos una empresa encargada a la gestion de proyectos tanto de pequeña como de gran escala, además de poder tener todo de menra ordenada, también ofrecemos multiples sistemas para los que requieran una gestión más personalizada.
-                        </p>
-                        <p>
-                            Proporcionamos sistemas especializados, seguimiento y una escalabilidad para que sus proyectos puedan empezar asi no se tenga una clara idea de que tanto recursos necesitará, nuestra infraestructura estará preparada para ello.
-                        </p>
+                    <div className="public__card1--contenido">
+                        <div className="public__card1--tittle">
+                            <h1>Proyectos S.A.S</h1>
+                        </div>
+                        <div className="public__card1--text">
+                            <p>
+                                Somos una empresa encargada a la gestion de proyectos tanto de pequeña como de gran escala, además de poder tener todo de menra ordenada, también ofrecemos multiples sistemas para los que requieran una gestión más personalizada.
+                            </p>
+                            <p>
+                                Proporcionamos sistemas especializados, seguimiento y una escalabilidad para que sus proyectos puedan empezar asi no se tenga una clara idea de que tanto recursos necesitará, nuestra infraestructura estará preparada para ello.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className="public__card2">
@@ -26,9 +28,9 @@ const Public = () => {
                     </div>
                 </div>
             </div>
-            <div style={{height: "150px", overflow: "hidden"}}>
-                <svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: "100%", width: "100vw"}}>
-                    <path d="M-89.72,-7.30 C67.72,266.02 255.08,58.80   499.44,133.80 L497.75,174.25 L-14.10,173.27 Z" style={{stroke: "none", fill: "#08f"}}></path>
+            <div style={{ height: "150px", overflow: "hidden" }}>
+                <svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{ height: "100%", width: "100vw" }}>
+                    <path d="M-89.72,-7.30 C67.72,266.02 255.08,58.80   499.44,133.80 L497.75,174.25 L-14.10,173.27 Z" style={{ stroke: "none", fill: "#08f" }}></path>
                 </svg>
             </div>
             <Footer />
